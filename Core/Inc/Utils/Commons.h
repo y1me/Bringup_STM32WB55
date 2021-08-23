4d616abc1dbc019b85266b5f0d401f276e920120
pierre yameogo <pya@ffly4u.com>
2021-08-23 16:51:45 +0200

    Add Commons.h

---
+++ Core/Inc/Utils/Commons.h
@@ -0,0 +1,18 @@
+/*
+ * Commons.h
+ *
+ *  Created on: Aug 23, 2021
+ *      Author: yamefflylt
+ */
+
+#ifndef INC_UTILS_COMMONS_H_
+#define INC_UTILS_COMMONS_H_
+
+//#include "i2c.h"
+
+//extern state_i2c_t i2C_currState;
+//extern event_i2c_t i2c_event;
+
+//void I2C_StateMachine_RunIt(state_i2c_t *, event_i2c_t )
+
+#endif /* INC_UTILS_COMMONS_H_ */
